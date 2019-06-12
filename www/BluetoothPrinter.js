b65dae4d1463bef1d335122a849fd7224f994585
Fabio Martins <fabio.co.martins@gmail.com>
2019-06-12 19:19:56 -0300

    Update BluetoothPrinter.js

--- www/BluetoothPrinter.js
+++ www/BluetoothPrinter.js
@@ -16,8 +16,20 @@ var BTPrinter = {
    printText: function(fnSuccess, fnError, str){
       exec(fnSuccess, fnError, "BluetoothPrinter", "printText", [str]);
    },
-   printPoule: function(fnSuccess, fnError, poule){
-      exec(fnSuccess, fnError, "BluetoothPrinter", "printPoule", [poule]);
+   printPoule: function(fnSuccess, fnError, poule, paper_type){
+      exec(fnSuccess, fnError, "BluetoothPrinter", "printPoule", [poule,paper_type]);
+   },
+   printResult: function(fnSuccess, fnError, poule, paper_type){
+      exec(fnSuccess, fnError, "BluetoothPrinter", "printResult", [poule,paper_type]);
+   },
+   printReport: function(fnSuccess, fnError, poule, paper_type){
+      exec(fnSuccess, fnError, "BluetoothPrinter", "printReport", [poule,paper_type]);
+   },
+   printCanceled: function(fnSuccess, fnError, poule, paper_type){
+      exec(fnSuccess, fnError, "BluetoothPrinter", "printCanceled", [poule,paper_type]);
+   },
+   printPayment: function(fnSuccess, fnError, poule, paper_type){
+      exec(fnSuccess, fnError, "BluetoothPrinter", "printPayment", [poule,paper_type]);
    },
    printImage: function(fnSuccess, fnError, str){
       exec(fnSuccess, fnError, "BluetoothPrinter", "printImage", [str]);
